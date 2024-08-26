c69a0695ad4151cdbed322b02ec0054697023745
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-26 23:07:48 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class arraylist 
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String>Pasu=new ArrayList<String>();
+        Pasu.add("Asish");
+        Pasu.add("Manas");
+        Pasu.add("Manoj");
+        Pasu.remove("Manas");
+        System.out.println(Pasu);  
+    }   
+}
