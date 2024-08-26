4ff300577287a52ad7aa899b5a0cb1b2d101d754
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-26 23:07:29 +0530

    Add files via upload

---
+++ casting.java
@@ -0,0 +1,24 @@
+public class casting {
+
+    public static void main(String[] args) {
+        
+
+
+            byte a = 12;
+            int b = a;
+            
+            // int c = 120;
+            // byte d=c;
+            
+            int e = 120;
+            byte f = (byte) e;
+            
+            int k = 259;
+            byte t = (byte) k;
+            
+        System.out.println(b);
+        // System.out.println(d);
+        System.out.println(f);
+        System.out.println(t);
+    }
+}
