5eb5919e6b90a090a21347ef05e5da69f2dcf7f6
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-27 22:57:18 +0530

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,16 @@
+
+public class exception {
+    public static void main(String[] args) {
+    try{ int a[]={2,6,4,7};
+      System.out.println(a[5]);
+    }
+
+ catch (Exception e) {
+System.out.println(e);
+ }
+ finally {
+  System.out.println("hello");
+}
+
+}
+}
