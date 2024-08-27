28b8f3294463271af8a420784c1f2006b61d4d05
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-27 22:52:50 +0530

    Add files via upload

---
+++ fahrenhite_to_celcius.java
@@ -0,0 +1,9 @@
+public class fahrenhite_to_celcius 
+{
+    public static void main(String [] args)
+    {
+        int f=35;
+        float C=(f-32)*5/9;
+        System.out.println("Equivalent celcius is = "+C);
+    }
+}
