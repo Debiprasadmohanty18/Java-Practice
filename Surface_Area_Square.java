55b4a19c3799da8795450655f58b1c1818f4f20a
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-27 22:51:58 +0530

    Add files via upload

---
+++ Surface_Area_Square.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Surface_Area_Square {
+    public static void main(String [] abd)
+    {
+     System.out.println("Enter Radius:-");
+     Scanner ab = new Scanner(System.in);
+     int r=ab.nextInt();
+     double A=4*3.14*r*r;
+     ab.close();
+     System.out.println("Area Of Rectangle is:-"+A);
+    }
+}
