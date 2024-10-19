3ace654eea79dd8ece4fdb870dc82dc232217237
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-10-19 08:02:18 +0530

    Add files via upload

---
+++ End_of_file.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class End_of_file {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 1;
+        while(sc.hasNext())
+        {
+            System.out.println(n + " "+ sc.nextLine());
+            n++;
+        }
+        sc.close();
+    }
+}
